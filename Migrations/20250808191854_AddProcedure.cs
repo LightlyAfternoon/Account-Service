06373a082b2b0afe35e39b1ecb633a5d0e3a59bc
LightlyAfternoon <asukca2@gmail.com>
2025-08-09 02:21:36 +0300

    Added simple accrue_interest procedure

---
+++ Migrations/20250808191854_AddProcedure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Account_Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // в плане вкладов работает скорее как сложные проценты для накопительного счёта, для более сложной логики нужны доп свойства для счетов
+            migrationBuilder.Sql("CREATE PROCEDURE accrue_interest(account_id UUID)\r\n" +
+                                 "LANGUAGE plpgsql\r\n" +
+                                 "AS $$\r\n" +
+                                 "DECLARE\r\n" +
+                                 "    v_rate NUMERIC;\r\n" +
+                                 "BEGIN\r\n" +
+                                 "    SELECT interestRate INTO v_rate FROM Accounts WHERE id = account_id;\r\n" +
+                                 "    UPDATE Accounts SET balance = balance + (balance * (v_rate / 100) / 365) WHERE id = account_id;\r\n" +
+                                 "    COMMIT;\r\n" +
+                                 "END;\r\n" +
+                                 "$$;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE accrue_interest");
+        }
+    }
+}
